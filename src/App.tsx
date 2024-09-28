58a010c8fd3973ea8dfcace9c044784106231a4e
Bon Nguyen <anh.xk.nguyen@gmail.com>
2024-09-28 19:10:58 -0400

    form validation + error message added

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,10 @@ function App() {
       <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
         <input
           {...register("email", {
-            required: true,
-            validate: (value) => value.includes("@"),
+            required: "Email is required",
+            validate: (value) => {
+              return !value.includes("@") ? "Email must include @" : true;
+            },
           })} //registered email variable to the form in FormFields
           className="border border-black"
           type="text"
@@ -34,15 +36,24 @@ function App() {
         {errors.email && (
           <div className="text-red-500">{errors.email.message}</div>
         )}
+
         <input
-          {...register("password", { required: true, minLength: 8 })} //registered password variable to the form in FormFields
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          })} //registered password variable to the form in FormFields
           className="border border-black"
           type="password"
           placeholder="Password"
         />
+
         {errors.password && (
           <div className="text-red-500">{errors.password.message}</div>
         )}
+
         <button className="border bg-zinc-200 hover:bg-zinc-500" type="submit">
           Submit
         </button>
