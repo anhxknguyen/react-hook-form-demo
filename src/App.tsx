602147efa5d8a87e1fe182e3986e2981ad91f62e
Bon Nguyen <anh.xk.nguyen@gmail.com>
2024-09-29 00:38:19 -0400

    zod added to streamline process

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import "./App.css";
+import { fromZodError } from "zod-validation-error";
 
-type FormFields = {
-  email: string;
-  password: string;
-};
+const schema = z.object({
+  email: z.string().email("This aint no email"),
+  password: z.string().min(8),
+});
+
+type FormFields = z.infer<typeof schema>;
+
+// type FormFields = {
+//   email: string;
+//   password: string;
+// };
 
 function App() {
   //register function takes in the name of the input field and an object containing validation rules
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormFields>();
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    defaultValues: { email: "test@gmail.com" },
+    resolver: zodResolver(schema),
+  });
 
-  const onSubmit: SubmitHandler<FormFields> = (data) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      throw new Error();
+      console.log(data);
+    } catch (error) {
+      setError("root", { message: "This email is already taken" });
+    }
   };
 
   return (
     <div className="w-1/5 mt-10 ml-10">
       <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
         <input
-          {...register("email", {
-            required: "Email is required",
-            validate: (value) => {
-              return !value.includes("@") ? "Email must include @" : true;
-            },
-          })} //registered email variable to the form in FormFields
+          {...register("email")} //registered email variable to the form in FormFields
           className="border border-black"
           type="text"
           placeholder="Email"
@@ -54,9 +69,16 @@ function App() {
           <div className="text-red-500">{errors.password.message}</div>
         )}
 
-        <button className="border bg-zinc-200 hover:bg-zinc-500" type="submit">
-          Submit
+        <button
+          disabled={isSubmitting}
+          className="border bg-zinc-200 hover:bg-zinc-500"
+          type="submit"
+        >
+          {isSubmitting ? "Submitting..." : "Submit"}
         </button>
+        {errors.root && (
+          <div className="text-red-500">{errors.root.message}</div>
+        )}
       </form>
     </div>
   );
